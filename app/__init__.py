1e3b97b4e0c1e7eb5ab3b3cd8cbcfe9bd6becf59
simengzhu <siz122@eng.ucsd.edu>
2018-10-05 14:50:54 -0700

    Set up bootstrap for the pages

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_type): # dev, test, prod
     app = Flask(__name__)
     configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
     app.config.from_pyfile(configuration)
 
-    db.init_app(app)
+    db.init_app(app) # bind database to app
+    bootstrap.init_app(app) # initialize bootstrap
 
     from app.catalog import main # import blueprint
     app.register_blueprint(main) # register blueprint
