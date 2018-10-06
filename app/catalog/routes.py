d9392cd183cc4fc84274ed764ccc4e484bc75019
simengzhu <siz122@eng.ucsd.edu>
2018-10-06 16:38:48 -0700

    Added movie deletion function

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,7 +1,8 @@
 from app.catalog import main
 from app import db
 from app.catalog.models import Movie, Studio
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
+from flask_login import login_required
 
 @main.route('/')
 def display_movies():
@@ -17,3 +18,16 @@ def display_studio(studio_id):
     studio_movies = Movie.query.filter_by(studio_id=studio.id).all()
 
     return render_template('studio.html', studio=studio, studio_movies=studio_movies)
+
+
+@main.route('/movie/delete/<movie_id>', methods=['GET', 'POST'])
+@login_required
+def delete_movie(movie_id):
+    movie = Movie.query.get(movie_id)
+    if request.method == 'POST':
+        db.session.delete(movie)
+        db.session.commit()
+        flash('movie deleted successfully')
+        return redirect(url_for('main.display_movies'))
+
+    return render_template('delete_movie.html', movie=movie, movie_id=movie.id)
