8dd568d2f7771166ba8aabc99fd8550eae70c124
simengzhu <siz122@eng.ucsd.edu>
2018-10-06 22:24:01 -0700

    Changing SQLAlchemy URI

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'DRxVxjUwSq_bigsql'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:DRxVxjUwSq_bigsql@localhost/catalog_db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:DRxVxjUwSq_bigsql@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(user_name='tyler').first():
-            User.create_user(user='tyler',
-                             email='tyler@durden.com',
-                             password='soaps')
-    flask_app.run()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='tyler').first():
+        User.create_user(user='tyler',
+                         email='tyler@durden.com',
+                         password='soaps')
+        flask_app.run()
